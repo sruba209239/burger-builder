8c4a8e71658b0b5ac57e915914c81c25605069c6
Sruba <sruba209239@gmail.com>
2020-09-30 00:18:39 +0530

    some corrections

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,11 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import classes from "./Burger.module.css";
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      console.log(igKey);
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
+  const transformedIngredients = Object.keys(props.ingredients).map((igKey) =>
+    [...Array(props.ingredients[igKey])].map((_, i) => (
+      <BurgerIngredient key={igKey + i} type={igKey} />
+    ))
+  );
 
   return (
     <div className={classes.Burger}>
