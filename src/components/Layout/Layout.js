47e582e3ddc988ce2d9c1b93c4c5b0acf48ad9c0
Sruba <sruba209239@gmail.com>
2020-11-13 01:58:58 +0530

    146

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import Auxiliary from '../../hoc/Auxiliary';
-import classes from './Layout.module.css';
+import React from "react";
+import Auxiliary from "../../hoc/Auxiliary";
+import classes from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const layout = (props) => (
-    <Auxiliary>
-        <Toolbar />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Auxiliary>
+  <Auxiliary>
+    <Toolbar />
+    <SideDrawer />
+    <main className={classes.Content}>{props.children}</main>
+  </Auxiliary>
 );
 
 export default layout;
