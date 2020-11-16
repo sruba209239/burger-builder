cb85e738d8d4987fae9dbedb28ce8f0ff3883033
Sruba <sruba209239@gmail.com>
2020-11-16 19:44:00 +0530

    149

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,19 +1,28 @@
 import React from "react";
+import Auxiliary from "../../../hoc/Auxiliary";
 import Logo from "../../Logo/Logo";
+import BackDrop from "../../UI/Backdrop/Backdrop";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import classes from "./SideDrawer.module.css";
 
 const sideDrawer = (props) => {
-  // ..
+  let sideDrawerClasses = [classes.SideDrawer, classes.Close];
+  if (props.open) {
+    sideDrawerClasses = [classes.SideDrawer, classes.Open];
+  }
+
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <Auxiliary>
+      <BackDrop show={props.open} backdropClicked={props.closeHandler} />
+      <div className={sideDrawerClasses.join(" ")}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Auxiliary>
   );
 };
 
